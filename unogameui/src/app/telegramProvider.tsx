2939db84cf0346791f31fd6680c23a0e35ddea90
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-11-01 19:54:00 +0530

    fix build issue

---
+++ unogameui/src/app/telegramProvider.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { type PropsWithChildren, useEffect, useMemo, useState } from "react";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
+
+import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { ErrorPage } from "@/components/ErrorPage";
+import { useTelegramMock } from "../hooks/useTelegramMock";
+import { useDidMount } from "../hooks/useDidMount";
+import Image from "next/image";
+import { init } from '@telegram-apps/sdk-react';
+
+
+function RootInner({ children }: PropsWithChildren) {
+  // Mock Telegram environment in development mode if needed.
+  if (process.env.NODE_ENV === "development") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTelegramMock();
+  }
+
+  const debug = useLaunchParams().startParam === "debug";
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    if (debug) {
+      import("eruda").then((lib) => lib.default.init());
+    }
+  }, [debug]);
+
+  init();
+
+  return (
+    <>
+      {children}
+    </>
+  );
+}
+
+export function TelegramProvider(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of the Server Side
+  // Rendering. That's why we are showing loader on the server side.
+  const didMount = useDidMount();
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props} />
+    </ErrorBoundary>
+  ) : (
+    <div className=" absolute top-0 left-0 flex flex-col items-center  gap-4 justify-center w-full h-full">
+      <Image src={"/butterfly.svg"} alt="Rabble" width={38} height={64} />
+      <div className="flex gap-4">
+        <p>Loading</p>
+        <Image
+          src={"/loader.svg"}
+          alt="loader"
+          width={24}
+          height={24}
+          className=" animate-spin"
+        />
+      </div>
+    </div>
+  );
+}
